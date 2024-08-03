37fe6a32996649c4367fedb4e900f32348319b4f
fardeen-andgate <fardeen093@gmail.com>
2024-08-03 23:09:25 +0500

    everything fixed

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -8,8 +8,8 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="https://nadeemsons-commerce-git-main-fardeens-projects.vercel.app/stripe/success"
-      cancelUrl="https://nadeemsons-commerce-git-main-fardeens-projects.vercel.app//stripe/error"
+      successUrl="https://nadeemsons-commerce.vercel.app/stripe/success"
+      cancelUrl="https://nadeemsons-commerce.vercel.app/stripe/error"
       currency="SAR"
       billingAddressCollection={true}
       shouldPersist={true}
